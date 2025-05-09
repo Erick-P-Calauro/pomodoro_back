96d946f9587578f730728eac76644d19b560fd4a
Erick-P-Calauro <erickcalauro@gmail.com>
2025-05-09 17:41:01 -0400

    feat: first commit and Task model

---
+++ src/main/java/com/anonymous/pomodoro_backend/Models/Project.java
@@ -0,0 +1,8 @@
+package com.anonymous.pomodoro_backend.Models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Project {
+    
+}
---
+++ src/main/java/com/anonymous/pomodoro_backend/Models/Task.java
@@ -0,0 +1,36 @@
+package com.anonymous.pomodoro_backend.Models;
+
+import java.util.UUID;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    private String title;
+
+    private String description;
+
+    @NotNull
+    private int productivityDone;
+
+    @NotNull
+    private int productivityGoal;
+}
